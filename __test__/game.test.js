57a35c3e11f3123cd9a7f5c5443d1b2d183212a2
Joy-Marchattiwar <92324931+Joy-Marchattiwar@users.noreply.github.com>
2026-07-25 16:08:02 +0530

    Refactor test to remove done callback

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,10 +2,9 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
- it('Contains the compiled JavaScript', async () => { // Removed (done)
-  const data = await fs.promises.readFile('./public/main.js', 'utf8')
-  expect(data).toMatchSnapshot()
-})
+  it('Contains the compiled JavaScript', async () => {
+    const data = await fs.promises.readFile('./public/main.js', 'utf8')
+    expect(data).toMatchSnapshot()
   })
 })
 
